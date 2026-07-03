ca1cba0075b6ce27585be3af05471e0bf47a762c
40303629 <40303629@ads.qub.ac.uk>
2026-07-03 17:18:11 +0100

    modified

---
+++ date.now.py
@@ -0,0 +1,30 @@
+# %%
+import datetime
+
+
+time = datetime.datetime.now()
+time
+
+# %%
+datetime.datetime(2009, 1, 6)
+print(str(time))
+
+
+# %%
+import datetime
+
+time = datetime.datetime.now()
+print(time)
+
+# %%
+custom_date = datetime.datetime(2009, 1, 6, 15, 8, 24, 78915)
+print(str(custom_date))
+
+
+# %%
+
+import sys
+print(sys.executable)
+# %%
+import streamlit
+# %%
